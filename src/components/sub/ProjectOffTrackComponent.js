d64a023fd00a5b094c21c86a261c02e1533e7f4f
sammydowds <sammydowds1993@gmail.com>
2020-06-11 11:36:37 -0500

    Converting to functional components for Off track, recent, and WIP panels

--- src/components/sub/ProjectOffTrackComponent.js
+++ src/components/sub/ProjectOffTrackComponent.js
@@ -11,67 +11,63 @@ import {
  } from 'reactstrap';
 import { Loading } from './LoadingComponent'; 
 
-class ProjectOff extends Component {
-  constructor(props) {
-    super(props); 
-  }
 
-  renderProj(projects) {
-    const projects_rend = projects.map((project) => {
-      const on_schedule = project.status; 
-      return(
-        <span>
-          <p style={{fontweight: 'superbold', fontSize: "12px"}}>
-            <span>
-              <svg class="bi bi-flag-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="#ffa41b" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M3.5 1a.5.5 0 0 1 .5.5v13a.5.5 0 0 1-1 0v-13a.5.5 0 0 1 .5-.5z"/>
-                <path fill-rule="evenodd" d="M3.762 2.558C4.735 1.909 5.348 1.5 6.5 1.5c.653 0 1.139.325 1.495.562l.032.022c.391.26.646.416.973.416.168 0 .356-.042.587-.126a8.89 8.89 0 0 0 .593-.25c.058-.027.117-.053.18-.08.57-.255 1.278-.544 2.14-.544a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-.5.5c-.638 0-1.18.21-1.734.457l-.159.07c-.22.1-.453.205-.678.287A2.719 2.719 0 0 1 9 9.5c-.653 0-1.139-.325-1.495-.562l-.032-.022c-.391-.26-.646-.416-.973-.416-.833 0-1.218.246-2.223.916A.5.5 0 0 1 3.5 9V3a.5.5 0 0 1 .223-.416l.04-.026z"/>
-              </svg> 
-              &nbsp; 
-              <a className="project-link" onClick={() => {this.props.handleProjectClicked(project);}}>
-                {project.name}
-              </a>
-            </span>
-          </p>  
-        </span>  
-      ); 
-    }); 
-    return (
+function renderProj(projects, handleClicked) {
+  const projects_rend = projects.map((project) => {
+    const on_schedule = project.status; 
+    return(
       <span>
-        {projects_rend}
-      </span>
+        <p style={{fontweight: 'superbold', fontSize: "12px"}}>
+          <span>
+            <svg class="bi bi-flag-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="#ffa41b" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M3.5 1a.5.5 0 0 1 .5.5v13a.5.5 0 0 1-1 0v-13a.5.5 0 0 1 .5-.5z"/>
+              <path fill-rule="evenodd" d="M3.762 2.558C4.735 1.909 5.348 1.5 6.5 1.5c.653 0 1.139.325 1.495.562l.032.022c.391.26.646.416.973.416.168 0 .356-.042.587-.126a8.89 8.89 0 0 0 .593-.25c.058-.027.117-.053.18-.08.57-.255 1.278-.544 2.14-.544a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-.5.5c-.638 0-1.18.21-1.734.457l-.159.07c-.22.1-.453.205-.678.287A2.719 2.719 0 0 1 9 9.5c-.653 0-1.139-.325-1.495-.562l-.032-.022c-.391-.26-.646-.416-.973-.416-.833 0-1.218.246-2.223.916A.5.5 0 0 1 3.5 9V3a.5.5 0 0 1 .223-.416l.04-.026z"/>
+            </svg> 
+            &nbsp; 
+            <a className="project-link" onClick={() => {handleClicked(project);}}>
+              {project.name}
+            </a>
+          </span>
+        </p>  
+      </span>  
     ); 
-  }
+  }); 
+  return (
+    <span>
+      {projects_rend}
+    </span>
+  ); 
+}
 
-  render () {
-    if (this.props.projectsLoading) {
-      return(
-        <Loading />
-      );
-    } else {
-      const offtrack_projects = this.props.projects.filter((project) => project.status == false); 
-      return(
-        <Row>
-          <Col>
-            <Card className="mt-3 card-border">
-              <CardBody>
-                <CardTitle className="pl-2 normal-text text-center">
-                  <h6 lead>
-                    Off Track <Badge className="off-badge">{offtrack_projects.length}</Badge>
-                  </h6>
-                </CardTitle>
-                <CardSubtitle>
-                  <hr></hr>
-                </CardSubtitle>
-                <CardText className="font-weight-bold text-left pl-2">{this.renderProj(offtrack_projects)}</CardText>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      );
-    }
+function ProjectOff(props) {
+  if (props.projectsLoading) {
+    return(
+      <Loading />
+    );
+  } else {
+    const offtrack_projects = props.projects.filter((project) => project.status == false); 
+    return(
+      <Row>
+        <Col>
+          <Card className="mt-3 card-border">
+            <CardBody>
+              <CardTitle className="pl-2 normal-text text-center">
+                <h6 lead>
+                  Off Track <Badge className="off-badge">{offtrack_projects.length}</Badge>
+                </h6>
+              </CardTitle>
+              <CardSubtitle>
+                <hr></hr>
+              </CardSubtitle>
+              <CardText className="font-weight-bold text-left pl-2">{renderProj(offtrack_projects, props.handleProjectClicked)}</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    );
   }
 }
+
 
 export default ProjectOff; 
 
--- src/components/sub/ProjectWorkInProgressComponent.js
+++ src/components/sub/ProjectWorkInProgressComponent.js
@@ -12,105 +12,100 @@ import {
  } from 'reactstrap';
 import { Loading } from './LoadingComponent'; 
 
-class ProjectWorkInProgress extends Component {
-  constructor(props) {
-    super(props); 
-  }
+function renderPhases(active_phases) {
+  const active_phases_lst = active_phases.map((phase) => {
+    const phase_end = moment(phase.end); 
+    return(
+      <span className="px-1">
+        <Badge color="dark">
+          {phase.name} {phase_end.fromNow()}
+        </Badge>
+      </span>
+    ); 
+  }); 
+  return(
+    <span>{active_phases_lst}</span>
+  ); 
+}
 
-  renderPhases(active_phases) {
-    const active_phases_lst = active_phases.map((phase) => {
-      const phase_end = moment(phase.end); 
+function renderProj(projects, phases, handleClicked) {
+  if (projects) {
+    const projects_rend = projects.map((project) => {
+      const on_schedule = project.status;
+      // filtering for phases related to this project
+      const proj_phases = phases.filter((phase) => project.id === phase.projectId); 
+      const active_phases = proj_phases.filter((phase) => phase.active);
+      const phases_rend = renderPhases(active_phases); 
       return(
-        <span className="px-1">
-          <Badge color="dark">
-            {phase.name} {phase_end.fromNow()}
-          </Badge>
-        </span>
+        <span>
+          <p style={{fontweight: 'superbold'}}>
+            <span className="pr-2">
+                <svg class="bi bi-box-seam" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
+                </svg>
+                &nbsp; 
+                <span className="project-link" onClick={() => {handleClicked(project);}}>
+                  {project.name}
+                </span>
+              &nbsp; 
+
+            </span>
+            <p className="ml-4">
+              { on_schedule
+                ? <span>
+                    <Badge color="success">On Track</Badge>
+                  </span>
+                : <span>
+                    <Badge className="off-badge">Off Track</Badge>
+                  </span>
+              } | 
+              {phases_rend}
+            </p>
+            <p className="ml-4">
+            </p>
+          </p>  
+        </span>  
       ); 
     }); 
-    return(
-      <span>{active_phases_lst}</span>
+    return (
+      <span>
+        {projects_rend}
+      </span>
     ); 
-  }
-
-  renderProj(projects, phases) {
-    if (projects) {
-      const projects_rend = projects.map((project) => {
-        const on_schedule = project.status;
-        // filtering for phases related to this project
-        const proj_phases = phases.filter((phase) => project.id === phase.projectId); 
-        const active_phases = proj_phases.filter((phase) => phase.active);
-        const phases_rend = this.renderPhases(active_phases); 
-        return(
-          <span>
-            <p style={{fontweight: 'superbold'}}>
-              <span className="pr-2">
-                  <svg class="bi bi-box-seam" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
-                  </svg>
-                  &nbsp; 
-                  <span className="project-link" onClick={() => {this.props.handleProjectClicked(project);}}>
-                    {project.name}
-                  </span>
-                &nbsp; 
-  
-              </span>
-              <p className="ml-4">
-                { on_schedule
-                  ? <span>
-                      <Badge color="success">On Track</Badge>
-                    </span>
-                  : <span>
-                      <Badge className="off-badge">Off Track</Badge>
-                    </span>
-                } | 
-                {phases_rend}
-              </p>
-              <p className="ml-4">
-              </p>
-            </p>  
-          </span>  
-        ); 
-      }); 
-      return (
-        <span>
-          {projects_rend}
-        </span>
-      ); 
-    } else {
-      return(
-      <div>wtf</div>); 
-    }
-  }
-
-  render () {
-    if (this.props.projectsLoading) {
-      return(
-        <Loading />
-      );
-    } else {
-      return(
-        <Row>
-          <Col>
-            <Card className="mt-3 text-center card-border">
-              <CardBody className="text-left">
-                <CardTitle className="pl-2">
-                  <h4 lead>
-                    Open Projects: Work In Progress
-                  </h4>
-                </CardTitle>
-                <CardSubtitle className="pl-2">
-                  <hr></hr>
-                </CardSubtitle>
-                <CardText className="pt-2 pl-4 font-weight-bold">{this.renderProj(this.props.projects, this.props.phases)}</CardText>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      );
-    }
+  } else {
+    return(
+    <div>wtf</div>); 
   }
 }
+
+function ProjectWorkInProgress(props) {
+  if (props.projectsLoading) {
+    return(
+      <Loading />
+    );
+  } else {
+    return(
+      <Row>
+        <Col>
+          <Card className="mt-3 text-center card-border">
+            <CardBody className="text-left">
+              <CardTitle className="pl-2">
+                <h4 lead>
+                  Open Projects: Work In Progress
+                </h4>
+              </CardTitle>
+              <CardSubtitle className="pl-2">
+                <hr></hr>
+              </CardSubtitle>
+              <CardText className="pt-2 pl-4 font-weight-bold">{renderProj(props.projects, props.phases, props.handleProjectClicked)}</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    );
+  }
+}
+
 
 export default ProjectWorkInProgress; 
 
