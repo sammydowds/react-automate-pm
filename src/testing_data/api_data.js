2f0589d0fe8dd2189e01b2071f2229263d2b5bf7
sammydowds <sammydowds1993@gmail.com>
2020-07-04 09:07:29 -0500

    Creating testing data folder

---
+++ src/testing_data/api_data.js
@@ -0,0 +1,189 @@
+export const state_empty = {
+    projects: {
+        isLoading: false,
+        errMess: false,
+        projects: []
+    }, 
+    phases: {
+        isLoading: false,
+        errMess: false,
+        phases: []
+    }, 
+    log: {
+        isLoading: false,
+        errMess: false, 
+        log: []
+    }, 
+    userinterface: {
+        projectDetails: {
+            open: false, 
+            projectId: null
+        }, 
+        phaseUpdateModal: {
+        open: false, 
+        phaseId: null
+        },
+        projectUpdateModal: {
+        open: false, 
+        projectId: null
+        }, 
+        projectCreateModal: {
+        open: false 
+        }, 
+        phaseCreateModal: {
+        open: false 
+        },
+        deleteModal: {
+        open: false
+        }
+    },
+    user: {
+        isLoading: false, 
+        errMess: false,
+        user: {
+            authenticated: false, 
+            username: null, 
+            token: null, 
+            loggedout: true, 
+            accountcreated: false, 
+            firstname: null
+        }
+        
+    }
+}
+
+export const state_small = {
+    projects: {
+        isLoading: false,
+        errMess: false,
+        projects: {
+            26: {
+                complete: false, 
+                id: 26, 
+                lastupdated: "2020-06-30T18:23:01.553000Z", 
+                name: "Django REST API - Projectile", 
+                owner: 71
+            }
+        }
+    }, 
+    phases: {
+        isLoading: false,
+        errMess: false,
+        phases: {
+            42: {
+                active: true, 
+                complete: false, 
+                end: "2020-06-29", 
+                id: 42, 
+                lastupdated: "2020-06-30T20:22:57.072000Z", 
+                name: "Engineering", 
+                projectId: 26, 
+                start: "2020-06-28"
+            }
+        }
+    }, 
+    log: {
+        isLoading: false,
+        errMess: false, 
+        log: []
+    }, 
+    userinterface: {
+        projectDetails: {
+            open: true, 
+            projectId: 26
+        }, 
+        phaseUpdateModal: {
+        open: false, 
+        phaseId: null
+        },
+        projectUpdateModal: {
+        open: false, 
+        projectId: null
+        }, 
+        projectCreateModal: {
+        open: false 
+        }, 
+        phaseCreateModal: {
+        open: false 
+        },
+        deleteModal: {
+        open: false
+        }
+    },
+    user: {
+        isLoading: false, 
+        errMess: false,
+        user: {
+            authenticated: false, 
+            username: null, 
+            token: null, 
+            loggedout: true, 
+            accountcreated: false, 
+            firstname: null
+        }
+        
+    }
+}
+
+export const state_nophases = {
+    projects: {
+        isLoading: false,
+        errMess: false,
+        projects: {
+            26: {
+                complete: false, 
+                id: 26, 
+                lastupdated: "2020-06-30T18:23:01.553000Z", 
+                name: "Django REST API - Projectile", 
+                owner: 71
+            }
+        }
+    }, 
+    phases: {
+        isLoading: false,
+        errMess: false,
+        phases: {
+        }
+    }, 
+    log: {
+        isLoading: false,
+        errMess: false, 
+        log: []
+    }, 
+    userinterface: {
+        projectDetails: {
+            open: true, 
+            projectId: 26
+        }, 
+        phaseUpdateModal: {
+        open: false, 
+        phaseId: null
+        },
+        projectUpdateModal: {
+        open: false, 
+        projectId: null
+        }, 
+        projectCreateModal: {
+        open: false 
+        }, 
+        phaseCreateModal: {
+        open: false 
+        },
+        deleteModal: {
+        open: false
+        }
+    },
+    user: {
+        isLoading: false, 
+        errMess: false,
+        user: {
+            authenticated: false, 
+            username: null, 
+            token: null, 
+            loggedout: true, 
+            accountcreated: false, 
+            firstname: null
+        }
+        
+    }
+}
